d9b3e7ddbd04d4612039d6e1c4f60f7f065c3b7a
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-03-02 16:29:34 -0300

    0204 - Fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Acesso } from '@/components/acesso'
+import { ClientFetch } from '@/components/client-fetch'
 
 export default async function Home() {
   return (
     <main>
       <h1>Home</h1>
       <Acesso />
+      <ClientFetch />
     </main>
   )
 }
